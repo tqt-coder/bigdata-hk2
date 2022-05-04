3dba5a9b1edaf41b2dafb7221c2fe42fe7eedf7b
tranquoctuan2001 <76949884+tranquoctuan2001@users.noreply.github.com>
2022-05-04 09:28:58 +0700

    file readme and edit file query

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Website load kết quả từ Hive
+
+## 1. Chuẩn bị
+- Dữ liện là data_1.csv
+- Viết các lệnh thực thi như tạo table, query trong file /src/main/resources/data/SaleAnalysis.sh
+- Copy file vào máy Master
+- Tạo Controller để xử lý RestFul APT
+- Tao các file giao diện: index.html, Count.html, SalePrice.html
+- Tạo các file JS để gọi API
+## 2. Thực hiện
+- Dụng lệnh sh + tên file được copy vào máy master + port ( trên máy local )
+- Xem kết quả
--- src/main/resources/data/SalesAnalysis.sh
+++ src/main/resources/data/SalesAnalysis.sh
@@ -9,15 +9,17 @@ echo "----- Start Sales Analysis -----"
   wget http://$1:$2/download/data_1.csv
 
   # Loading data file into hive table
-  hive -e "LOAD DATA LOCAL INPATH 'data_1.csv' OVERWRITE INTO TABLE sales";
+  hive -e "LOAD DATA LOCAL INPATH 'data_1.csv'  INTO TABLE sales";
 
   # Executing the query and creating the result file.
   hive -e 'SELECT * FROM sales limit 10' > result_1
+    curl -X POST http://$1:$2/upload -H 'content-type: multipart/form-data' -F file=@result_1
+echo "----- complete query 1-----"
   hive -e 'SELECT product, count(product_id) FROM sales GROUP BY product' > result_2
+  curl -X POST http://$1:$2/upload2 -H 'content-type: multipart/form-data' -F file=@result_2
+echo "----- complete query 2-----"
   hive -e 'SELECT product, max(price) as MaxPrice FROM sales group by product order by MaxPrice desc limit 10' > result_3
   # Uploading the result file to the client system.
-  curl -X POST http://$1:$2/upload -H 'content-type: multipart/form-data' -F file=@result_1
-  curl -X POST http://$1:$2/upload2 -H 'content-type: multipart/form-data' -F file=@result_2
   curl -X POST http://$1:$2/upload3 -H 'content-type: multipart/form-data' -F file=@result_3
-
+echo "----- complete query 3-----"
 echo "----- End Sales Analysis -----"
